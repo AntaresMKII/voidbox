9bbe8db10511e083972b5b53c4c76312d148e6d2
Yannick Abouem <35614284+AntaresMKII@users.noreply.github.com>
2020-08-02 15:02:39 -0400

    Update voidbox-setup.sh

--- voidbox-setup.sh
+++ voidbox-setup.sh
@@ -12,7 +12,7 @@ sudo xbps-install -Syu
 #services
 sudo xbps-install -y dbus elogind xorg 
 #openbox and lightdm
-sudo xbps-install -y openbox obconf obmenu obmenu-generator tint2 nitrogen gtk+ lightdm lightdm-gtk-greeter-settings lxappearence picom polybar
+sudo xbps-install -y openbox obconf obmenu obmenu-generator tint2 nitrogen gtk+ lightdm lightdm-gtk-greeter-settings lxappearance picom polybar
 #zsh
 sudo xbps-install -y bash-completion zsh wget
 #ho-my-zsh
@@ -24,7 +24,7 @@ sudo xbps-install -y connman connman-ui connman-gtk
 #audio
 sudo xbps-install -y alsa-utils alsa-plugins-pulseaudio pa-applet
 #other programs
-sudo xbps-install -y terminator thunar chromium rofi
+sudo xbps-install -y terminator Thunar chromium rofi
 
 
 #activate services
