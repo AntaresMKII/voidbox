0eebb388bead5245bc36ce0d4ed0135535884438
AntaresMkII <abouemyannick@gmail.com>
2020-08-15 13:53:57 -0400

    Added a bunch of stuff

--- ohmyzsh-install.sh
+++ ohmyzsh-install.sh
@@ -1,3 +1,6 @@
 #!bin/bash
 
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+rm ~/.zshrc
+cp ./dotfiles/zsh/.zshrc
--- voidbox-setup.sh
+++ voidbox-setup.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 #A script to install everithing you need to start
 
+echo 'Installing additional repositories for xbps'
+
 sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
+
+echo 'Changing repository mirror to New York'
 
 sudo mkdir -p /etc/xbps.d
 sudo cp /usr/share/xbps.d/*-repository-*.conf /etc/xbps.d/
 sudo sed -i 's|https://alpha.de.repo.voidlinux.org|https://mirror.clarkson.edu/voidlinux|g' /etc/xbps.d/*-repository-*.conf
 
+echo 'Updating the system'
+
 sudo xbps-install -Syu
 
 #services
+echo 'installing dbus elogind and xorg'
 sudo xbps-install -y dbus elogind xorg 
 #openbox and lightdm
+echo 'installing programs'
 sudo xbps-install -y openbox obconf obmenu obmenu-generator tint2 nitrogen gtk+ lightdm lightdm-gtk-greeter-settings lxappearance picom
 #zsh
 sudo xbps-install -y bash-completion zsh wget nano
@@ -22,14 +30,22 @@ sudo xbps-install -y connman connman-ui connman-gtk
 sudo xbps-install -y alsa-utils alsa-plugins-pulseaudio pa-applet
 #other programs
 sudo xbps-install -y terminator Thunar firefox rofi geany xterm
-#fonts
-sudo xbps-install -y fonts-roboto-ttf google-fonts-ttf
 
-#other
+#installation openbox
+echo 'setting up openbox and tint2'
+cp ./dotfiles/openbox/autostart ~/.config/openbox/
+rm ~/.config/openbox/rc.xml
+cp ./dotfiles/openbox/rc.xml ~/.config/openbox
+
+#openbox themes installation
+git clone https://github.com/addy-dclxvi/openbox-theme-collections ~/.themes
+
+#tint2 theme installation
+git clone https://github.com/addy-dclxvi/tint2-theme-collections ~/.config/tint2 --depth 1 
 
 
 
-
+echo 'activating services'
 #activate services
 sudo ln -s /etc/sv/dbus /var/service
 sudo ln -s /etc/sv/elogind /var/service
