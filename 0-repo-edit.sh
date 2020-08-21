d7e93a5ca0aa912f55c0474316c30de0b4d8c3c5
yannick <abouemyannick@gmial.com>
2020-08-21 15:57:51 -0400

    changed a few tings no time to write commit messages

--- 0-repo-edit.sh
+++ 0-repo-edit.sh
@@ -24,24 +24,24 @@ mirrors[us_newyork]=https://mirror.clarkson.edu/voidlinux/current				#tier 1 New
 mirrors[as_almaty]=https://mirror.ps.kz/voidlinux/current						#tier 2 Almaty KZ
 mirrors[as_china1]=https://mirrors.bfsu.edu.cn/voidlinux/current				#tier 2 China
 mirrors[as_china2]=https://mirrors.cnnic.cn/voidlinux/current					#tier 2 China
-mirrors[as_china3]=https://mirrors.tuna.tsinghua.edu.cn/voidlinux/current	#tier 2 China
-mirrors[as_seoul]=https://mirror.maakpain.kro.kr/void/current				#tier 2 Seoul SK
-mirrors[as_singapore]=https://void.webconverger.org/current					#tier 2 Singapore
-mirrors[au_canberra]=https://mirror.aarnet.edu.au/pub/voidlinux/current		#tier 2 Canberra AU
-mirrors[au_melbourne]=https://ftp.swin.edu.au/voidlinux/current				#tier 2 Melbourne AU
-mirrors[eu_amsterdam]=https://void.cijber.net/current						#tier 2 Amsterdam
-EU_DENMARK1=http://dk.archive.ubuntu.com/voidlinux/current			#tier 2 Denmark
-EU_DENMARK2=http://ftp.dk.xemacs.org/voidlinux/current				#tier 2 Denmark
-EU_DENMARK3=https://mirrors.dotsrc.org/voidlinux/current			#tier 2 Denmark
-EU_HUNGARY=https://quantum-mirror.hu/mirrors/pub/voidlinux/current	#tier 2 Hungary
-EU_IRELAND=https://mirror.i-novus.ru/mirrors/voidlinux/current		#tier 2 Ireland
-EU_RUSSIA1=http://ftp.debian.ru/mirrors/voidlinux/current			#tier 2 Russia
-EU_RUSSIA2=https://mirror.yandex.ru/mirrors/voidlinux/current		#tier 2 Russia
-EU_SWEDEN1=https://cdimage.debian.org/mirror/voidlinux/current		#tier 2 Sweden
-EU_SWEDEN2=https://ftp.acc.umu.se/mirror/voidlinux/current			#tier 2 Sweden
-EU_SWEDEN3=https://ftp.gnome.org/mirror/voidlinux/current			#tier 2 Sweden
-EU_SWEDEN4=https://ftp.lysator.liu.se/pub/voidlinux/current			#tier 2 Sweden
-EU_SWEDEN5=https://ftp.sunet.se/mirror/voidlinux/current			#tier 2 Sweden
+mirrors[as_china3]=https://mirrors.tuna.tsinghua.edu.cn/voidlinux/current		#tier 2 China
+mirrors[as_seoul]=https://mirror.maakpain.kro.kr/void/current					#tier 2 Seoul SK
+mirrors[as_singapore]=https://void.webconverger.org/current						#tier 2 Singapore
+mirrors[au_canberra]=https://mirror.aarnet.edu.au/pub/voidlinux/current			#tier 2 Canberra AU
+mirrors[au_melbourne]=https://ftp.swin.edu.au/voidlinux/current					#tier 2 Melbourne AU
+mirrors[eu_amsterdam]=https://void.cijber.net/current							#tier 2 Amsterdam
+mirrors[eu_denmark1]=http://dk.archive.ubuntu.com/voidlinux/current				#tier 2 Denmark
+mirrors[eu_denmark2]=http://ftp.dk.xemacs.org/voidlinux/current					#tier 2 Denmark
+mirrors[eu_denmark3]=https://mirrors.dotsrc.org/voidlinux/current				#tier 2 Denmark
+mirrors[eu_hungary]=https://quantum-mirror.hu/mirrors/pub/voidlinux/current		#tier 2 Hungary
+mirrors[eu_ireland]=https://mirror.i-novus.ru/mirrors/voidlinux/current			#tier 2 Ireland
+mirrors[eu_russia1]=http://ftp.debian.ru/mirrors/voidlinux/current				#tier 2 Russia
+mirrors[eu_russia2]=https://mirror.yandex.ru/mirrors/voidlinux/current			#tier 2 Russia
+mirrors[eu_sweden1]=https://cdimage.debian.org/mirror/voidlinux/current			#tier 2 Sweden
+mirrors[eu_sweden2]=https://ftp.acc.umu.se/mirror/voidlinux/current				#tier 2 Sweden
+mirrors[eu_sweden3]=https://ftp.gnome.org/mirror/voidlinux/current				#tier 2 Sweden
+mirrors[eu_sweden4]=https://ftp.lysator.liu.se/pub/voidlinux/current			#tier 2 Sweden
+mirrors[eu_sweden5]=https://ftp.sunet.se/mirror/voidlinux/current				#tier 2 Sweden
 
 #=======================================================================
 
@@ -49,13 +49,8 @@ echo "==============================================\n"
 echo "Mirror selection\n"
 echo "Select the mirror nearest to you\n"
 echo "==============================================\n"
-echo "Tier 1 mirrors:\n"
-echo "Finland\tChicago, US\tKansas City, US\tNew York, US\n"
-echo "==============================================\n"
-echo "Tier 2 mirrors:\n"
-echo "Almaty, KZ\tChina 1\tChina 2\tChina 3\tSeoul, SK\tSingapore\tCanberra, AU\tMelbourne, AU\tAmsterdam\tDenmark 1\tDenmark 2\tDenmark 3\tHungary\tIreland\tRussia 1\tRussia 2\tSweden 1\tSweden 2\tSweden 3\tSweden 4\tSweden 5\n"
-echo "\nEnter the name of the mirror:"
-read ans
-echo $ans
+for key in "${!mirrors[@]}"; do
+	echo $key
+done
 
 
